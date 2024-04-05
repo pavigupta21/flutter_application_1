a7d13b94d778127ed3ce57f0000ec18440c91d79
Pavi Gupta <pavigupta21@gmail.com>
2024-04-06 02:14:12 +0530

    Forgot pass

--- lib/Pages/FP.dart
+++ lib/Pages/FP.dart
@@ -9,10 +9,11 @@ class ForgotPass extends StatefulWidget {
 }
 
 class _ForgotPassState extends State<ForgotPass> {
+  String username = "", answer = "";
   TextEditingController _usernameController = TextEditingController();
   TextEditingController _answerController = TextEditingController();
   String _selectedItem = 'Who is your favourite author?';
-
+  final _formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     var size;
@@ -33,106 +34,121 @@ class _ForgotPassState extends State<ForgotPass> {
         ),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            SizedBox(height: height * 0.04),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: width * 0.05),
-              child: TextField(
-                controller: _usernameController,
-                decoration: InputDecoration(
-                  labelText: 'Username',
-                  filled: true,
-                  fillColor: Colors.white,
+        child: Form(
+          key: _formkey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SizedBox(height: height * 0.04),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: width * 0.05),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Please enter Username";
+                    }
+                    return null;
+                  },
+                  controller: _usernameController,
+                  decoration: InputDecoration(
+                    labelText: 'Username',
+                    filled: true,
+                    fillColor: Colors.white,
+                  ),
+                  style: TextStyle(fontSize: width * 0.04),
                 ),
-                style: TextStyle(fontSize: width * 0.04),
               ),
-            ),
-            SizedBox(height: height * 0.03),
-            Text(
-              'Secret Question',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: width * 0.04,
+              SizedBox(height: height * 0.03),
+              Text(
+                'Secret Question',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: width * 0.04,
+                ),
               ),
-            ),
-            SizedBox(height: height * 0.02),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: width * 0.05),
-              child: DropdownButton<String>(
-                value: _selectedItem,
-                onChanged: (String? newValue) {
-                  setState(() {
-                    _selectedItem = newValue!;
-                  });
+              SizedBox(height: height * 0.02),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: width * 0.05),
+                child: DropdownButton<String>(
+                  value: _selectedItem,
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      _selectedItem = newValue!;
+                    });
+                  },
+                  items: <String>[
+                    'Who is your favourite author?',
+                    'Which is your favourite book?',
+                    "What was your first pet's name?",
+                    "What is the last name of your best friend?",
+                    "Who is your favourite movie star?",
+                    "What town were you born in?",
+                    "What is your favourite restaurant?",
+                    "Who is your favourite historical person?",
+                    "What is the name of your eldest cousin?",
+                    "What is your favourite travelling destination?",
+                    "What is your favourite sport?",
+                    "What was your favourite food as a child?",
+                    "What is your favourite hobby?",
+                    'What is your favourite car model?',
+                  ].map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(
+                        value,
+                        style: TextStyle(fontSize: width * 0.03),
+                      ),
+                    );
+                  }).toList(),
+                ),
+              ),
+              SizedBox(height: height * 0.02),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: width * 0.05),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "This field is required";
+                    }
+                    return null;
+                  },
+                  controller: _answerController,
+                  decoration: InputDecoration(
+                    labelText: 'Answer to the secret question',
+                    filled: true,
+                    fillColor: Colors.white,
+                  ),
+                  style: TextStyle(fontSize: width * 0.04),
+                ),
+              ),
+              SizedBox(height: height * 0.03),
+              ElevatedButton(
+                onPressed: () {
+                  if (_selectedItem.isNotEmpty &&
+                      _answerController.text.isNotEmpty) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ForgotPassNew()),
+                    );
+                    String username = _usernameController.text;
+                    String selectedItem = _selectedItem;
+                    String answer = _answerController.text;
+                    // You can use the variables as needed.
+                  } else {
+                    // Show an error message or handle the case where no option is selected or answer is provided.
+                  }
                 },
-                items: <String>[
-                  'Who is your favourite author?',
-                  'Which is your favourite book?',
-                  "What was your first pet's name?",
-                  "What is the last name of your best friend?",
-                  "Who is your favourite movie star?",
-                  "What town were you born in?",
-                  "What is your favourite restaurant?",
-                  "Who is your favourite historical person?",
-                  "What is the name of your eldest cousin?",
-                  "What is your favourite travelling destination?",
-                  "What is your favourite sport?",
-                  "What was your favourite food as a child?",
-                  "What is your favourite hobby?",
-                  'What is your favourite car model?',
-                ].map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(
-                      value,
-                      style: TextStyle(fontSize: width * 0.03),
-                    ),
-                  );
-                }).toList(),
-              ),
-            ),
-            SizedBox(height: height * 0.02),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: width * 0.05),
-              child: TextField(
-                controller: _answerController,
-                decoration: InputDecoration(
-                  labelText: 'Answer to the secret question',
-                  filled: true,
-                  fillColor: Colors.white,
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.blueAccent,
+                  disabledForegroundColor: Colors.white,
                 ),
-                style: TextStyle(fontSize: width * 0.04),
+                child: Text(
+                  'Next',
+                  style: TextStyle(fontSize: width * 0.04),
+                ),
               ),
-            ),
-            SizedBox(height: height * 0.03),
-            ElevatedButton(
-              onPressed: () {
-                if (_selectedItem.isNotEmpty &&
-                    _answerController.text.isNotEmpty) {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => ForgotPassNew()),
-                  );
-                  String username = _usernameController.text;
-                  String selectedItem = _selectedItem;
-                  String answer = _answerController.text;
-                  // You can use the variables as needed.
-                } else {
-                  // Show an error message or handle the case where no option is selected or answer is provided.
-                }
-              },
-              style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.blueAccent,
-                disabledForegroundColor: Colors.white,
-              ),
-              child: Text(
-                'Next',
-                style: TextStyle(fontSize: width * 0.04),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
